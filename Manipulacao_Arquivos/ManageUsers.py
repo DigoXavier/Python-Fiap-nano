9c89d3bcef0d29072071f1967e48701d2abb93d9
DigoXavier <rodrigox52@icloud.com>
2022-07-19 19:34:51 -0300

    commit manipulação arquivos e JSON

--- Manipulacao_Arquivos/ManageUsers.py
+++ Manipulacao_Arquivos/ManageUsers.py
@@ -1,8 +1,8 @@
 from Dicionarios.Funcoes import perguntar, inserir
 
 usuarios = {}
-opcao = perguntar
+opcao = perguntar()
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
         inserir(usuarios)
-    opcao=perguntar
+    opcao=perguntar()
---
+++ Manipulacao_Arquivos/arquivos.py
@@ -0,0 +1,21 @@
+import json
+
+dicionario = {
+"CHAVES": [
+    "CHAVES DO 8",
+    "14/04/2017",
+    "REC_01"
+  ],
+  "QUICO": [
+    "QUICO FLORES",
+    "24/04/2017",
+    "RAIOX_01"
+  ],
+  "FLORINDA": [
+    "DONA FLORINDA",
+    "18/04/2017",
+    "REC_01"
+  ]
+}
+with open("bd1.json", "w") as json_file:
+    json.dump(dicionario,json_file)
